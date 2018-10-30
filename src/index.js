5daab2625c0702659395fb68a709c0426a1ced7a
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-10-30 11:37:11 -0400

    added Provider and Router to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './store'
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={ store }>
+  <Router>
+    <App />
+  </Router>
+</Provider>, document.getElementById('root'));
 
 
 serviceWorker.unregister();
