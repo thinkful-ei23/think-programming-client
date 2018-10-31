d81df70bca1107e37f44963a5f565251b5e41264
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-10-31 17:26:00 -0400

    config update

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL =
     process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
-export const API_BASE_URL_SOCKET = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
+export const API_BASE_URL_SOCKET = process.env.REACT_APP_API_BASE_URL_SOCKET || 'http://localhost:8080';
