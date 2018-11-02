584c59e5293dd034a6ab29b6817abfa1f3037b1f
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-11-02 10:33:36 -0400

    non working namespacing

--- src/components/game-room.js
+++ src/components/game-room.js
@@ -3,10 +3,10 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import io from "socket.io-client";
 import { API_BASE_URL_SOCKET } from '../config';
+// import './game-room.css';
 import { fetchQuestions  } from '../actions/questions';
 import { enterGameRoom } from '../actions/game-room';
 
-import './styles/game-room.css';
 class GameRoom extends Component {
   constructor(props) {
     super(props);
@@ -18,62 +18,63 @@ class GameRoom extends Component {
         meFinished: false,
         challengerFinished: false
       }
-	this.socket = io(API_BASE_URL_SOCKET);
-  this.socket.on('TYPING', (incoming) => {
-    if (incoming.username === this.props.username) {
-      this.setState({ myTyping: incoming.input });
-    } else {
-      this.setState({ challengerTyping: incoming.input })
-    }
-  });
-  this.socket.on('FINISHED', (incoming) => {
-    if (incoming.username === this.props.username) {
-      this.setState({
-        meFinished: true,
-        challengerFinished: false
+      this.socket = io.connect(`${API_BASE_URL_SOCKET}${this.props.location.pathname}`);
+  
+      this.socket.on('TYPING', (incoming) => {
+        console.log(incoming)
+        if (incoming.username === this.props.username) {
+          this.setState({ myTyping: incoming.input });
+        } else {
+          this.setState({ challengerTyping: incoming.input })
+        }
       });
-    } else {
-      this.setState({
-        meFinished: false,
-        challengerFinished: true })
-    }
-  });
-  this.socket.on('SIT', (incoming) => {
-    let currentPlayers = this.state.players;
-    if (incoming.username === this.props.username) {
-      this.setState({
-        meSitting: true,
-        players: currentPlayers += 1
+      this.socket.on('FINISHED', (incoming) => {
+        if (incoming.username === this.props.username) {
+          this.setState({
+            meFinished: true,
+            challengerFinished: false
+          });
+        } else {
+          this.setState({
+            meFinished: false,
+            challengerFinished: true })
+        }
       });
-    } else {
-      this.setState({
-        players: currentPlayers += 1 
-      })
-    }
-  });
-  this.socket.on('LEAVE', (incoming) => {
-    let currentPlayers = this.state.players;
-    if (incoming.username === this.props.username) {
-      this.setState({
-        meSitting: false,
-        players: currentPlayers -= 1
+      this.socket.on('SIT', (incoming) => {
+        let currentPlayers = this.state.players;
+        if (incoming.username === this.props.username) {
+          this.setState({
+            meSitting: true,
+            players: currentPlayers += 1
+          });
+        } else {
+          this.setState({
+            players: currentPlayers += 1 
+          })
+        }
       });
-    } else {
-      this.setState({
-        players: currentPlayers -= 1 
-      })
-    }
-  });
+      this.socket.on('STAND', (incoming) => {
+        let currentPlayers = this.state.players;
+        if (incoming.username === this.props.username) {
+          this.setState({
+            meSitting: false,
+            players: currentPlayers -= 1
+          });
+        } else {
+          this.setState({
+            players: currentPlayers -= 1 
+          })
+        }
+      });
   }
   sendMessage(e){
     this.socket.emit('TYPING', { username: this.props.username, input: e.currentTarget.value });
   }
   sendSitOrStand = (props) => {
-    console.log(this.state)
     if (this.state.meSitting === false) {
       this.socket.emit('SIT', { username: this.props.username });
     } else {
-      this.socket.emit('LEAVE', { username: this.props.username })
+      this.socket.emit('STAND', { username: this.props.username })
     }
   }
   sendFinished() {
@@ -105,15 +106,13 @@ render() {
   return (
     <div className="game-room">
       <Link to='/dashboard'>Dashboard</Link>
-      <h1>GameRoom</h1>
-      <h2>{this.props.match.params.value}</h2>
+      <h2>GameRoom</h2>
       <h3>{questionTitle}</h3>
       <p>{question}</p>
       <div>{winner} Finished!</div>
-      <h4>Player 2</h4>
-      <div className='challenger-typing-area'>{this.state.challengerTyping}</div>
-      <button type="button">Sit</button>
       <h4>Player 1</h4>
+      <div className='challenger-typing-area'>{this.state.challengerTyping}</div>
+      <h4>Player 2</h4>
       <textarea className='my-typing-area' type="text" onChange={e => this.sendMessage(e)} rows="4" cols="50"/>
       <button type="button" onClick={this.sendSitOrStand}>{sitOrLeave}</button>
       <button className='finished-button' onClick={() => this.sendFinished()}>Finished</button>
