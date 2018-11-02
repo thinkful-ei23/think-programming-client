79a9f820c50cea7f24966f5a3414aba85ade8d27
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-11-02 16:21:49 -0400

    app smoketest successful

---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './app';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe.only('<App />', () => {
+  it('Should render without crashing', () => {
+    shallow(<App />);
+  });
+});
