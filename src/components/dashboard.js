614f80942c5f3521433e1657e29f2d401e28fb16
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-11-03 14:34:30 -0400

    added socket to dashboard and header bar, on dashboard we can see userCount, when user logs out socket receives the username who logged out

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { API_BASE_URL_SOCKET } from '../config';
+import io from "socket.io-client";
 import requiresLogin from './requires-login';
 import './styles/dashboard.css';
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        totalUserCount: 0
+      }
+    this.socket = io.connect(`${API_BASE_URL_SOCKET}/dashboard`);
+    this.socket.on('USERS', userArray => {
+      let totalUsers = userArray.length;
+      this.setState({totalUserCount: totalUsers});
+    });
+  }
+  sendNewUser() {
+    this.socket.emit('USERS', this.props.username)
+  }
+  componentDidMount() {
+    this.sendNewUser();
+  }
   render() {
     let questionTypes = [ ['jsQuestions','JavaScript'], ['htmlQuestions', 'HTML'], ['cssQuestions', 'CSS'], ['dsaQuestions', 'Data Structures & Algorithms' ]];
     let rooms = questionTypes.map((questionArray, i) => {
@@ -14,9 +33,14 @@ class Dashboard extends Component {
         </Link>
       );
     });
+    let coders = 'Coder';
+    if (this.state.totalUserCount > 1) {
+      coders = 'Coders';
+    }
     return (
       <div className="dashboard">
         <h2>Challenge Rooms</h2>
+        <h4>{this.state.totalUserCount} {coders} Online</h4>
         <div className="gamerooms">
           {rooms}
         </div>
--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { clearAuth } from '../actions/auth';
 import{ clearAuthToken } from "../local-storage";
-
+import { API_BASE_URL_SOCKET } from '../config';
+import io from "socket.io-client";
 import tplogo from './images/logo/TP-logo.jpg';
 import './styles/header-bar.css';
 
 class HeaderBar extends Component {
+    socket = io.connect(`${API_BASE_URL_SOCKET}/dashboard`);
     logOut() {
+        this.socket.emit('USER_EXIT', this.props.auth.currentUser.username);
         this.props.dispatch(clearAuth());
         clearAuthToken();
     }
-
     render() {
         let logOutButton;
         if (this.props.loggedIn) {
@@ -28,8 +30,11 @@ class HeaderBar extends Component {
     }
 }
 
-const mapStateToProps = state => ({
-    loggedIn: state.auth.currentUser !== null,
-});
+const mapStateToProps = state => {
+    return {
+        auth: state.auth,
+        loggedIn: state.auth.currentUser !== null,
+    }
+};
 
 export default connect(mapStateToProps)(HeaderBar);
