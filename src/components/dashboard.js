f07a70af307a20fc4565e2f99722b6dad3391ef7
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-11-05 18:26:23 -0500

    added playerCount to dashboard.js

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -10,26 +10,37 @@ class Dashboard extends Component {
   constructor(props) {
     super(props);
       this.state = {
-        totalUserCount: 0
+        totalUserCount: 0,
+        allUsers: {}
       }
     this.socket = io.connect(`${API_BASE_URL_SOCKET}/dashboard`);
     this.socket.on('USERS', userArray => {
       let totalUsers = userArray.length;
       this.setState({totalUserCount: totalUsers});
     });
+    this.socket.on('ALL_PLAYERS', incomingRoomObject => {
+      this.setState({allUsers: incomingRoomObject})
+    });
   }
   sendNewUser() {
     this.socket.emit('USERS', this.props.username)
   }
+  getAllUsers() {
+    this.socket.emit('ALL_PLAYERS', this.props.username) 
+  }
   componentDidMount() {
     this.sendNewUser();
+    this.getAllUsers();
+  }
+  componentDidUpdate() {
+    this.getAllUsers();
   }
   render() {
-    let questionTypes = [ ['jsQuestions','JavaScript'], ['htmlQuestions', 'HTML'], ['cssQuestions', 'CSS'], ['dsaQuestions', 'Data Structures & Algorithms' ]];
+    let questionTypes = [ ['jsQuestions','JavaScript', 'jsPlayers'], ['htmlQuestions', 'HTML', 'htmlPlayers'], ['cssQuestions', 'CSS', 'cssPlayers'], ['dsaQuestions', 'Data Structures & Algorithms', 'dsaPlayers']];
     let rooms = questionTypes.map((questionArray, i) => {
       return (
         <Link className={`room-link-container-${i}`} key={i} to={`/gameroom${i}/${questionArray[0]}`}>
-          {questionArray[1]}
+          {questionArray[1]}: {this.state.allUsers[questionArray[2]] ? this.state.allUsers[questionArray[2]].length : 0} Of 2 Players Ready
         </Link>
       );
     });
@@ -53,6 +64,7 @@ const mapStateToProps = state => {
   return {
       username: state.auth.currentUser.username,
       name: currentUser.name,
+      gameRoom: state.gameRoom
   };
 };
 
--- src/reducers/game-room.js
+++ src/reducers/game-room.js
@@ -1,28 +1,35 @@
 import {
+  IN_GAMEROOM,
   ENTER_GAMEROOM,
   LEAVE_GAMEROOM
 } from '../actions/game-room';
 
 const initialState = {
-  gameRoom0Count: 0,
-  gameRoom1Count: 0,
-  gameRoom2Count: 0,
-  gameRoom3Count: 0
+  jsQuestions: 0,
+  htmlQuestions: 0,
+  cssQuestions: 0,
+  dsaQuestions: 0
 };
 
 export default function gameRoomReducer(state = initialState, action) {
+  if (action.type === IN_GAMEROOM) {
+    let numOfPlayers = action.playerArray.length;
+    let roomType = action.roomType;
+    return Object.assign({}, state, {
+      [roomType]: numOfPlayers
+    });
+  };
   if (action.type === ENTER_GAMEROOM) {
-    console.log('attempted')
     let room = `gameRoom${action.roomNumber}Count`
     return Object.assign({}, state, {
       [room]: state[room] += 1
     });
-  }
-  else if (action.type === LEAVE_GAMEROOM) {
+  };
+  if (action.type === LEAVE_GAMEROOM) {
     let room = `gameRoom${action.roomNumber}Count`
     return Object.assign({}, state, {
       [room]: state[room] -= 1
     });
-  }
+  };
   return state;
 }
