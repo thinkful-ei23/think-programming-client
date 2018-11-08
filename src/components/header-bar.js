4490343fb2405072cc4abf2dd3f632a57aefa715
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-11-08 09:03:27 -0500

    fixed header-bar logout warning by adding promise of socket emit.then clear auth token

--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -8,26 +8,31 @@ import tplogo from './images/logo/TP-logo.jpg';
 import './styles/header-bar.css';
 
 class HeaderBar extends Component {
-    socket = io.connect(`${API_BASE_URL_SOCKET}/dashboard`);
-    logOut() {
-        this.socket.emit('USER_EXIT', this.props.auth.currentUser.username);
-        this.props.dispatch(clearAuth());
-        clearAuthToken();
-    }
-    render() {
-        let logOutButton;
-        if (this.props.loggedIn) {
-            logOutButton = (
-                <button onClick={() => this.logOut()} className="logout"> Log out</button>
-            );
-        }
-        return (
-            <div className="header-bar">
-                <img src={tplogo} className="header-bar-img" alt="Think Programming Logo" />
-                {logOutButton}
-            </div>
-        );
-    }
+    		
+	socket = io.connect(`${API_BASE_URL_SOCKET}/dashboard`);
+	logOut() {
+		return Promise.all([
+			this.socket.emit('USER_EXIT', this.props.auth.currentUser.username)
+		])
+		.then(() => {
+			this.props.dispatch(clearAuth());
+					clearAuthToken();
+		});
+	}
+	render() {
+		let logOutButton;
+		if (this.props.loggedIn) {
+			logOutButton = (
+				<button onClick={() => this.logOut()} className="logout"> Log out</button>
+			);
+		}
+		return (
+			<div className="header-bar">
+				<img src={tplogo} className="header-bar-img" alt="Think Programming Logo" />
+				{logOutButton}
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = state => {
