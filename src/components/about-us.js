b1df83941e207f53a189ceed11bec7166da954b2
Timothy Dao <timster78811@gmail.com>
2018-11-13 12:59:00 -0500

    updated aboutus

--- src/components/about-us.js
+++ src/components/about-us.js
@@ -6,15 +6,24 @@ import './styles/about-us.css';
 export default function AboutUs() {
   return (
     <div className="about-us">
-      <div className="about-us-header"> 
-        <h2>About Us</h2> 
+      <div className="about-us-header">
+        <h2>About Us</h2>
       </div>
       <div className="about-us-content">
-        <p>A Place for coders to hone their skills and challenge another enthusiest in a language or programing category. Answer the fastest and cleanest correct code and win.  But watch out becuase you can see each others code as it being written so cheating is allowed.</p>
-        <br/>
-        <p>Think Programming can improve fundamental coding muscle memory</p>
+        <p>
+          Think Programming is a code type-racing game to improve your skills to
+          be a more efficient coder, whether you are an experienced or beginner
+          software engineer.One way to improve is to practice writing more lines
+          of code. Why not learn and have competitive fun at the same time?
+          Everyone loves friendly competition. We help you display your coding
+          prowess by competing against other coders in real time.{' '}
+        </p>
+        <br />
+        {/* <p>Think Programming can improve fundamental coding muscle memory</p> */}
       </div>
-      <Link to="/" className="back">Back</Link>
+      <Link to="/" className="back">
+        Back
+      </Link>
     </div>
   );
 }
