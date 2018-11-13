0666666cc5ee1a4a0a8eb3b2f380f839227b332f
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-11-13 11:55:59 -0500

    removed input.test.js

--- src/components/input.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Input from './input';
-
-describe('<Input />', () => {
-  it('Should render without crashing', () => {
-    shallow(<Input />);
-  });
-});
