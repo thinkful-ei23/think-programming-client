1c98b1cb72cc4a62bbde9c8f73564e67b24c6fd8
Jon Sung <jonsung89@gmail.com>
2018-11-15 02:11:05 -0600

    Fixed typo in aboutus and changed styling

--- src/components/about-us.js
+++ src/components/about-us.js
@@ -13,13 +13,12 @@ export default function AboutUs() {
         <p>
           Think Programming is a code type-racing game to improve your skills to
           be a more efficient coder, whether you are an experienced or beginner
-          software engineer.One way to improve is to practice writing more lines
+          software engineer. One way to improve is to practice writing more lines
           of code. Why not learn and have competitive fun at the same time?
           Everyone loves friendly competition. We help you display your coding
           prowess by competing against other coders in real time.{' '}
         </p>
         <br />
-        {/* <p>Think Programming can improve fundamental coding muscle memory</p> */}
       </div>
       <Link to="/" className="back">
         Back
