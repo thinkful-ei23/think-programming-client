18666929fde5fd875bc366c0e0424af8216f65f1
Timothy Dao <timster78811@gmail.com>
2018-12-07 12:26:44 -0500

    added oneliner to landingpage

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 import Footer from './footer';
 // Styling
 import './styles/landing-page.css';
-import logo from './images/logo/TP-logo.png'
+import logo from './images/logo/TP-logo.png';
 
 export class LandingPage extends Component {
   render() {
@@ -13,14 +13,21 @@ export class LandingPage extends Component {
     }
     return (
       <div className="landing">
-        <img src={logo} alt={"Think Programming"} />
+        <img src={logo} alt={'Think Programming'} />
         <h1 className="app-name">Think Programming</h1>
+        <h3 className="oneliner">Challenge another coder and see who wins!</h3>
         <div className="whoarewe">
-          <Link to ="/aboutus" className="btn aboutus">About Us</Link>
+          <Link to="/aboutus" className="btn aboutus">
+            About Us
+          </Link>
         </div>
         <div className="directory">
-          <Link to="/login" className="btn reg-log">Sign In</Link>
-          <Link to="/register" className="btn reg-log">Register</Link>   
+          <Link to="/login" className="btn reg-log">
+            Sign In
+          </Link>
+          <Link to="/register" className="btn reg-log">
+            Register
+          </Link>
         </div>
         <Footer />
       </div>
